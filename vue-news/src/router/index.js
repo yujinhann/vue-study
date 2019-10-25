f600db5fa63b19051748529737177b9245b4fc10
yujin <h.yujin@mrblue.com>
2019-10-25 15:36:12 +0900

    #20191025 고급 편 프로젝트 생성, router, vue 파일구조

---
+++ vue-news/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        {
+            // path : url 주소
+            path: '/news',
+            // component : url 주소로 갔을 때 표시될 컴포넌트
+            component: NewsView,
+        },
+        {
+            path: '/ask',
+            component: AskView,
+        },
+        {
+            path: '/jobs',
+            component: JobsView,
+        },
+    ]
+});
