055781e09bc190d522315cfe3f9145194f371c65
yujin <h.yujin@mrblue.com>
2019-10-29 17:14:30 +0900

    #20191029 리팩토링1 - 리스트 아이템 컴포넌트 공통화'

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,66 +1,15 @@
 <template>
     <div>
-        <ul class="news-list">
-            <li v-for="job in fetchedJobs" class="post">
-                <!-- 포인트 영역 -->
-                <div class="points">
-                    {{ job.points || 0 }}
-                </div>
-                <!-- 기타 정보 영역 -->
-                <div>
-                    <p class="news-title">
-                        <a :href="job.url">{{ job.title }}</a>
-                    </p>
-                    <small class="link-text">
-                        {{ job.time_ago }} by
-                        <a :href="job.url">
-                            {{ job.domain }}
-                        </a>
-                    </small>
-                </div>
-            </li>
-        </ul>
+        <list-item></list-item>
     </div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex';
+    import ListItem from '../components/ListItem.vue';
 
     export default {
-        computed: {
-            ...mapGetters([
-                'fetchedJobs'
-            ])
-        },
-        created() {
-            this.$store.dispatch('FETCH_JOBS');
+        components: {
+            ListItem,
         },
     }
 </script>
-
-<style scoped>
-    .news-list {
-        margin: 0;
-        padding: 0;
-    }
-    .post {
-        list-style: none;
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid #eee;
-    }
-    .points {
-        width: 80px;
-        height: 60px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #42b883;
-    }
-    .news-title {
-        margin: 0;
-    }
-    .link-text {
-        color: #828282;
-    }
-</style>
