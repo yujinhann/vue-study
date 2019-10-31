ae94d9f16b932dbda3a26d79d870e210c8915bb3
yujin <h.yujin@mrblue.com>
2019-10-31 17:52:52 +0900

    #20191031 외부 라이브러리 모듈화 방법'

---
+++ vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart';
+import LineChart from "./components/LineChart";
+
+export default {
+  //컴포넌트 속성 && 인스턴스 옵션
+  components: {
+    BarChart,
+    LineChart
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-charts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,32 @@
+<template>
+    <canvas ref="lineChart" id="lineChart"></canvas>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            var lineChart = new this.$_Chart(this.$refs.lineChart.getContext('2d'), {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 10, 5, 2, 20, 30, 45]
+                    }]
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
