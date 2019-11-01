2be78ad47723766701e603936722223ac4d9da7c
yujin <h.yujin@mrblue.com>
2019-11-01 18:12:57 +0900

    #20191101 컴포넌트 디자인 패턴 - Controlled Component

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <check-box v-model="checked"></check-box>
+</template>
+
+<script>
+  import CheckBox from "./components/CheckBox";
+
+export default {
+  components: {
+    CheckBox,
+  },
+  data() {
+    return {
+      checked: false,
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
