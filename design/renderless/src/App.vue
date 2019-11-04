7ccbe8ec34a68c4fc62fd0221dc2b9fdefb5c5d1
yujin <h.yujin@mrblue.com>
2019-11-04 15:16:31 +0900

    #20191104 컴포넌트 디자인 패턴 : Renderless COmponent

---
+++ design/renderless/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <div slot-scope="{ response, loading }">
+        <div v-if="!loading">
+          <p>name : {{ response.name }}</p>
+          <p>email : {{ response.email }}</p>
+        </div>
+        <div v-else>
+          Loading...
+        </div>
+      </div>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from "./components/FetchData";
+
+export default {
+  components: {
+    FetchData,
+  }
+}
+</script>
+
+<style>
+</style>
